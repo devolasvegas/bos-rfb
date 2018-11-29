b6884e7b34d80af5296b081c9f63c006606bdff8
Gel Studio <devondaviau@yahoo.ca>
2018-11-29 16:29:07 -0500

    edit header, create inventory and order components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { fips } from 'crypto';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
 
 class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
-        <p>Heeeyyyyy</p>
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
       </div>
     )
   }
