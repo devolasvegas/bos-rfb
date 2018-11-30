74be99e66897a48910586c7eeda4ab8cc0b18158
Gel Studio <devondaviau@yahoo.ca>
2018-11-30 11:49:05 -0500

    create loadSampleFishes method to update state with sample fishes

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
+import sampleFishes from '../sample-fishes';
+
 class App extends React.Component {
 
   state = {
@@ -22,6 +24,10 @@ class App extends React.Component {
     })
   }
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -29,7 +35,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={ this.addFish } />
+        <Inventory addFish={ this.addFish } loadSampleFishes={ this.loadSampleFishes } />
       </div>
     )
   }
