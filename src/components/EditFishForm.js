8975c610df9cb56c24cafac97208712f88eda360
Gel Studio <devondaviau@yahoo.ca>
2018-11-30 15:40:12 -0500

    create edit fish form component

---
+++ src/components/EditFishForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddFishForm from './AddFishForm';
+
+class EditFishForm extends React.Component {
+  render() {
+    return (
+      <p>Edit Fish</p>
+    )
+  }
+}
+
+export default EditFishForm;
